0efc9d8541e9f2f961ea4e2ca0841acd41b96d53
yukiyamamuro <yuki.sumaho@gmail.com>
2022-10-04 17:34:04 +0900

    setup

--- swap_contract/hardhat.config.ts
+++ swap_contract/hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    testnet_aurora: {
+      url: "https://testnet.aurora.dev",
+      accounts: [process.env.AURORA_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
