05b962bc19598c4dca919e7968986a097128d2ee
yukiyamamuro <yuki.sumaho@gmail.com>
2022-10-05 18:00:08 +0900

    chore(contract): add contract code

---
+++ swap_contract/contracts/Swap.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SwapContract {
+  address public deployerAddress;
+
+  constructor() payable {
+    deployerAddress = msg.sender;
+  }
+
+  function calculateValue(address tokenSendAddress, address tokenRecieveAddress) public view returns (uint256 value) {
+    value = (1 ether) * IERC20(tokenSendAddress).balanceOf(address(this)) / ERC20(tokenRecieveAddress).balanceOf(address(this));
+  }
+
+  function distributeToken(address tokenAddress, uint256 amount, address recipientAddress) public {
+    require(msg.sender == deployerAddress, "Anyone but deployer can distribute token");
+    IERC20 token = IERC20(tokenAddress);
+    token.transfer(recipientAddress, amount);
+  }
+
+  function swap(
+    address sendTokenAddress,
+    address measureTokenAddress,
+    address receiveTokenAddress,
+    uint256 amount,
+    address recipientAddress
+  ) public payable {
+    IERC20 sendToken = IERC20(sendTokenAddress);
+    IERC20 receiveToken = IERC20(receiveTokenAddress);
+
+    uint256 sendTokenValue = calculateValue(sendTokenAddress, measureTokenAddress);
+    uint256 recieveTokenValue = calculateValue(receiveTokenAddress, measureTokenAddress);
+    uint256 sendAmount = amount * sendTokenValue / (1 ether);
+    uint256 recieveAmount = amount * recieveTokenValue / (1 ether);
+
+    require(sendToken.balanceOf(msg.sender) >= sendAmount, "Your asset is smaller than amount you want to send");
+    require(receiveToken.balanceOf(address(this)) >= recieveAmount, "Contract asset of the currency recipient want is smaller than amount you want to send");
+
+    sendToken.transferFrom(msg.sender, address(this), sendAmount);
+    receiveToken.transfer(recipientAddress, recieveAmount);
+  }
+}
